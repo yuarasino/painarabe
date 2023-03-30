846e2eea1551037993ff58188c9021f33fa0d407
新篠ゆう <37145593+yuarasino@users.noreply.github.com>
2023-03-30 14:44:28 +0900

    :mahjong: 一旦完成

---
+++ README.md
@@ -0,0 +1,10 @@
+# painarabe
+
+(個人用) 牌を並べて表示するツール
+
+## License
+
+[MIT](./LICENSE.md)
+
+牌の画像
+https://majandofu.com/mahjong-images
---
+++ script.js
@@ -0,0 +1,51 @@
+(() => {
+  const $form = document.querySelector("#form")
+  const $input = document.querySelector("#input")
+  const $target = document.querySelector("#target")
+  const $output = document.querySelector("#output")
+
+  const suitMap = {
+    m: "man",
+    p: "pin",
+    s: "sou",
+    z: "ji",
+  }
+
+  $form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    const tiles = []
+    const hand = $input.value.replace(/r5/g, "0")
+    const regex = /[0-9]+[mpsz]/g
+    const matches = hand.match(regex)
+    matches.forEach((match) => {
+      const suit = match.slice(-1)
+      const ranks = match.slice(0, -1).split("")
+      ranks.forEach((rank) => {
+        tiles.push(rank + suit)
+      })
+    })
+    $target.innerHTML = ""
+    tiles.forEach((tile) => {
+      const rank = tile[0]
+      const suit = tile[1]
+      const src = `${suitMap[suit]}${rank}-66-90-l-emb.png`
+      $target.insertAdjacentHTML(
+        'beforeend',
+        `<img src="./tiles/${src}" alt="${tile}" />`,
+      )
+    })
+
+    $output.innerHTML = ""
+    const options = {
+      width: 66 * tiles.length,
+      backgroundColor: "transparent",
+    }
+    html2canvas($target, options).then((canvas) => {
+      const src = canvas.toDataURL("image/png")
+      $output.insertAdjacentHTML(
+        'beforeend',
+        `<img src="${src}" alt="${hand}.png" />`,
+      )
+    })
+  })
+})()
